c67de11b64ccc20a66656854cb919db294154a4b
Emerson Knapp <emerson.b.knapp@gmail.com>
2019-10-11 00:40:01 -0700

    Readloop starting to come together

--- neato_botvac/neato_driver2.py
+++ neato_botvac/neato_driver2.py
@@ -12,78 +12,44 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import asyncio
-import threading
+import sys
 import time
 import traceback
-import sys
+from typing import List, Tuple
 
 import serial
 from serial.threaded import LineReader, ReaderThread
 
 
-class BotvacConnectionError(Exception):
-    pass
+def _boolstr(val: bool) -> str:
+    return 'on' if val else 'off'
 
 
-class BotvacDriver():
-    def __init__(self, port: str = '/dev/ttyACM0', baud: int = 115200):
-        self.port = serial.serial(port, baud, timeout=0.1)
-        if not self.port.isOpen():
-            raise BotvacConnectionError('Could not open connection to the botvac')
-        self.info('Connection established')
-
-        self.readThread = threading.Thread(target=self.read, daemon=True)
-
-    def err(self, msg: str):
-        print(msg)
-
-    def info(self, msg: str):
-        print(msg)
-
-
-# class Output(asyncio.Protocol):
-#     def connection_made(self, transport):
-#         self.transport = transport
-#         print('port opened', transport)
-#         transport.serial.rts = False
-#         transport.write(b'getaccel\n')
-#
-#     def data_received(self, data):
-#         print('data received', repr(data))
-#         self.transport.close()
-#
-#     def connection_lost(self, exc):
-#         print('port closed')
-#         asyncio.get_event_loop().stop()
-
-
-# if __name__ == '__main__':
-    # loop = asyncio.get_event_loop()
-    # coro = serial.aio.create_serial_connection(loop, Output, '/dev/ttyUSB0', baudrate=115200)
-    # loop.run_until_complete(coro)
-    # loop.run_forever()
-    # loop.close()
-
-
-class PrintLines(LineReader):
-    def __init__(self, *args, **kwargs):
-        super(PrintLines, self).__init__(*args, **kwargs)
+class ResponseReader(LineReader):
+    def __init__(self, state_dict, *args, **kwargs):
+        super(ResponseReader, self).__init__(*args, **kwargs)
+        self.state = state_dict
         self.current = []
 
     def connection_made(self, transport):
-        super(PrintLines, self).connection_made(transport)
+        super(ResponseReader, self).connection_made(transport)
         sys.stdout.write('port opened\n')
-        self.write_line('help')
+
+    def _command_complete(self):
+        print('Received full message')
+        print(self.current)
+        for line in self.current:
+            tokens = line.split(',')
+            if len(tokens) > 1:
+                self.state[tokens[0]] = tokens[1]
+        print(self.state)
+        self.current = []
 
     def handle_line(self, data):
         if data == '\x1a':
-            print('Received full message\n')
-            print(self.current)
-            self.current = []
-            return
-        self.current.append(data)
-        # sys.stdout.write('line received: {}\n'.format(repr(data)))
+            self._command_complete()
+        else:
+            self.current.append(data)
 
     def connection_lost(self, exc):
         if exc:
@@ -91,12 +57,84 @@ class PrintLines(LineReader):
         sys.stdout.write('port closed\n')
 
 
+class BotvacDriver:
+    def __init__(self, port: str = '/dev/ttyACM0', baud: int = 115200):
+        self.state = {}
+        self._port = serial.Serial(port, baud, timeout=1)
+        self._reader = ReaderThread(self._port, self._get_reader)
+        self._reader.start()
+        self._reader._connection_made.wait()
+        if not self._reader.alive:
+            raise RuntimeError('connection_lost already called')
+        self._protocol = self._reader.protocol
+        self._base_width = 240  # mm
+        self._max_speed = 300  # mm/s
+        self._setTestMode(True)
+        time.sleep(0.5)  # TODO should be able to build this into our communications
+        self._setLDS(True)
+
+    def __del__(self):
+        self._setLDS(False)
+        self._setTestMode(False)
+        self._protocol.stop()
+
+    def _get_reader(self):
+        return ResponseReader(self.state)
+
+    @property
+    def baseWidth(self) -> int:
+        """Distance between wheel centers, in mm."""
+        return self._base_width
+
+    @property
+    def maxSpeed(self) -> int:
+        """Max speed of a robot wheel, in mm/s."""
+        return self._max_speed
+
+    def _setTestMode(self, on: bool) -> None:
+        self._protocol.write_line('testmode {}'.format(_boolstr(on)))
+
+    def _setLDS(self, on: bool) -> None:
+        self._protocol.write_line('setldsrotation {}'.format(_boolstr(on)))
+
+    def setMotors(self, left: int, right: int) -> None:
+        """Set the speed of the left and right wheel motors, in mm/s."""
+        pass
+
+    def getMotors(self) -> Tuple[int, int]:
+        """Return current left, right encoder values."""
+        pass
+
+    def getCharger(self):
+        """Return latest battery status."""
+        self._protocol.write_line('getcharger')
+
+    def getScanRanges(self) -> List[float]:
+        """
+        Return latest LDS info as a list of distances in mm.
+
+        It is expected to be 360 points with 1 degree between each, over 0.2 seconds.
+        Therefore time between samples is 0.2 / 360 ~= 0.56ms
+        """
+        pass
+
+
 def main():
-    ser = serial.serial_for_url('/dev/ttyACM0', baudrate=115200, timeout=1)
-    with ReaderThread(ser, PrintLines) as protocol:
-        while 1:
-            protocol.write_line('getldsscan')
-            time.sleep(0.2)
+    driver = BotvacDriver()
+    while 1:
+        driver.getCharger()
+        time.sleep(0.2)
+        print(driver.state)
+    # ser = serial.serial_for_url('/dev/ttyACM0', baudrate=115200, timeout=1)
+    # state = {}
+    #
+    # def getreader():
+    #     return ResponseReader(state)
+    #
+    # with ReaderThread(ser, getreader) as protocol:
+    #     while 1:
+    #         protocol.write_line('getcharger')
+    #         time.sleep(0.2)
 
 
 if __name__ == '__main__':
