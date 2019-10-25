edea7e612b8fae97cad97fcd175cb6101099cb59
Emerson Knapp <emerson.b.knapp@gmail.com>
2019-10-24 22:11:35 -0700

    continue wip reading commands

--- neato_botvac/neato_driver2.py
+++ neato_botvac/neato_driver2.py
@@ -15,7 +15,10 @@
 import sys
 import time
 import traceback
-from typing import List, Tuple
+from typing import Callable
+from typing import List
+from typing import NamedTuple
+from typing import Optional
 
 import serial
 from serial.threaded import LineReader, ReaderThread
@@ -25,50 +28,95 @@ def _boolstr(val: bool) -> str:
     return 'on' if val else 'off'
 
 
+class Encoders(NamedTuple):
+    stamp: float
+    left: int
+    right: int
+
+
+class BatteryStatus(NamedTuple):
+    stamp: float
+    voltage: float
+    temperature: float
+    current: float
+    percentage: int
+    present: bool
+
+
+class Scan(NamedTuple):
+    stamp: float
+    """
+    List of distances in mm.
+
+    It is expected to be 360 points with 1 degree between each, over 0.2 seconds.
+    Therefore time between samples is 0.2 / 360 ~= 0.56ms
+    """
+    ranges: List[int]
+
+
+class BotvacDriverCallbacks(NamedTuple):
+    encoders: Optional[Callable[[Encoders], None]]
+    battery: Optional[Callable[[BatteryStatus], None]]
+    scan: Optional[Callable[[Scan], None]]
+
+
 class ResponseReader(LineReader):
-    def __init__(self, state_dict, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(ResponseReader, self).__init__(*args, **kwargs)
-        self.state = state_dict
-        self.current = []
+        self.current_stamp = time.time()
+        self.current_lines = []
 
     def connection_made(self, transport):
         super(ResponseReader, self).connection_made(transport)
         sys.stdout.write('port opened\n')
-
-    def _command_complete(self):
-        print('Received full message')
-        print(self.current)
-        for line in self.current:
-            tokens = line.split(',')
-            if len(tokens) > 1:
-                self.state[tokens[0]] = tokens[1]
-        print(self.state)
-        self.current = []
-
-    def handle_line(self, data):
-        if data == '\x1a':
-            self._command_complete()
-        else:
-            self.current.append(data)
 
     def connection_lost(self, exc):
         if exc:
             traceback.print_exc(exc)
         sys.stdout.write('port closed\n')
 
+    def _command_complete(self):
+        print('Received full message at {}'.format(self.current_stamp))
+        state = {}
+
+        # interpret
+        for line in self.current_lines:
+            tokens = line.split(',')
+            if len(tokens) > 1:
+                state[tokens[0]] = tokens[1]
+        print(state)
+
+        # clean up
+        self.current_lines = []
+
+    def handle_line(self, data):
+        if not len(self.current_lines):
+            self.current_stamp = time.time()
+        if data == '\x1a':
+            self._command_complete()
+        else:
+            self.current_lines.append(data)
+
 
 class BotvacDriver:
-    def __init__(self, port: str = '/dev/ttyACM0', baud: int = 115200):
-        self.state = {}
+    def __init__(
+        self,
+        port: str = '/dev/ttyACM0',
+        baud: int = 115200,
+        callbacks: Optional[BotvacDriverCallbacks] = None,
+    ):
         self._port = serial.Serial(port, baud, timeout=1)
         self._reader = ReaderThread(self._port, self._get_reader)
         self._reader.start()
         self._reader._connection_made.wait()
+
         if not self._reader.alive:
             raise RuntimeError('connection_lost already called')
+
         self._protocol = self._reader.protocol
         self._base_width = 240  # mm
         self._max_speed = 300  # mm/s
+        self._callbacks = callbacks
         self._setTestMode(True)
         time.sleep(0.5)  # TODO should be able to build this into our communications
         self._setLDS(True)
@@ -76,10 +124,9 @@ class BotvacDriver:
     def __del__(self):
         self._setLDS(False)
         self._setTestMode(False)
-        self._protocol.stop()
 
     def _get_reader(self):
-        return ResponseReader(self.state)
+        return ResponseReader()
 
     @property
     def baseWidth(self) -> int:
@@ -97,34 +144,39 @@ class BotvacDriver:
     def _setLDS(self, on: bool) -> None:
         self._protocol.write_line('setldsrotation {}'.format(_boolstr(on)))
 
-    def setMotors(self, left: int, right: int) -> None:
+    def motorCommand(self, left_dist: int, right_dist: int, speed: int) -> None:
         """Set the speed of the left and right wheel motors, in mm/s."""
         pass
 
-    def getMotors(self) -> Tuple[int, int]:
-        """Return current left, right encoder values."""
-        pass
+    def requestEncoders(self):
+        self._protocol.write_line('getmotors')
 
-    def getCharger(self):
-        """Return latest battery status."""
+    def requestBattery(self) -> None:
+        """
+        Send a request for latest battery status.
+
+        If there is a registered callback for battery, it will be called when the values
+        are ready.
+        """
         self._protocol.write_line('getcharger')
 
-    def getScanRanges(self) -> List[float]:
+    def requestScan(self) -> None:
         """
-        Return latest LDS info as a list of distances in mm.
+        Send a request for latest LIDAR scan.
 
-        It is expected to be 360 points with 1 degree between each, over 0.2 seconds.
-        Therefore time between samples is 0.2 / 360 ~= 0.56ms
+        If there is a registered `scan` callback, it will be called when the values
+        are ready.
         """
-        pass
+        self._protocol.write_line('getldsscan')
 
 
 def main():
     driver = BotvacDriver()
     while 1:
-        driver.getCharger()
-        time.sleep(0.2)
-        print(driver.state)
+        driver.requestEncoders()
+        driver.requestBattery()
+        # driver.requestScan()
+        time.sleep(1)
     # ser = serial.serial_for_url('/dev/ttyACM0', baudrate=115200, timeout=1)
     # state = {}
     #
